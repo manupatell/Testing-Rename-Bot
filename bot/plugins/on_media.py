5d6db6a4038e40d3cc145c66747ed3289dddcf69
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-01-14 07:34:58 +0530

    Update on_media.py

--- bot/plugins/on_media.py
+++ bot/plugins/on_media.py
@@ -19,10 +19,12 @@ async def on_media_handler(c: Client, m: "types.Message"):
     await asyncio.sleep(3)
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text="**Should I show File Information?**",
+        text="**Select the Your wish to Execute the Process**",
         reply_markup=types.InlineKeyboardMarkup(
-            [[types.InlineKeyboardButton("Yes", callback_data="showFileInfo"),
-              types.InlineKeyboardButton("No", callback_data="closeMessage")]]
+            [[types.InlineKeyboardButton("Edit Metadata", callback_data="EditMetadata"),
+              types.InlineKeyboardButton("Rename File", callback_data="RenameFile"),
+              types.InlineKeyboardButton("Media Information", callback_data="showFileInfo")
+              types.InlineKeyboardButton("Not Now", callback_data="closeMessage")]]
         ),
         disable_web_page_preview=True,
         reply_to_message_id=m.message_id
