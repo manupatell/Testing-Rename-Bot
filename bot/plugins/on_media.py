a8b626230cdbd70d1010383065eaeb4bd2ef7113
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-01-14 12:57:08 +0530

    Update on_media.py

--- bot/plugins/on_media.py
+++ bot/plugins/on_media.py
@@ -8,26 +8,25 @@ from pyrogram import (
     types
 )
 
-
 @Client.on_message((filters.video | filters.audio | filters.document) & ~filters.channel & ~filters.edited)
 async def on_media_handler(c: Client, m: "types.Message"):
     if not m.from_user:
         return await m.reply_text("I Don't Know About you Bro/Sis..")
+    
     await add_user_to_database(c, m)
     await asyncio.sleep(3)
 
-    # Send all four buttons in a single message
     buttons = [
-        types.InlineKeyboardButton("Edit Metadata", callback_data="EditMetadata"),
-        types.InlineKeyboardButton("Rename File", callback_data="RenameFile"),
-        types.InlineKeyboardButton("Media Information", callback_data="showFileInfo"),
-        types.InlineKeyboardButton("Not Now", callback_data="closeMessage")
+        [types.InlineKeyboardButton("Edit Metadata", callback_data="EditMetadata")],
+        [types.InlineKeyboardButton("Rename File", callback_data="RenameFile")],
+        [types.InlineKeyboardButton("Media Information", callback_data="showFileInfo")],
+        [types.InlineKeyboardButton("Not Now", callback_data="closeMessage")]
     ]
     
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text="**Select the Your wish to Execute the Process**",
-        reply_markup=types.InlineKeyboardMarkup([buttons]),
+        text="**Select Your wish to Execute the Process**",
+        reply_markup=types.InlineKeyboardMarkup(buttons),
         disable_web_page_preview=True,
         reply_to_message_id=m.message_id
     )
