9dc51bc6b40d74445029e2ac621add4289d15367
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-01-14 12:20:29 +0530

    Update rename.py

--- bot/plugins/rename.py
+++ bot/plugins/rename.py
@@ -1,11 +1,7 @@
-# (c) @AbirHasan2005
-
 import time
 import mimetypes
 import traceback
-from bot.client import (
-    Client
-)
+from bot.client import Client
 from pyrogram import filters
 from pyrogram.file_id import FileId
 from pyrogram.types import Message
@@ -32,19 +28,13 @@ async def rename_handler(c: Client, m: Message):
     if m.from_user.id not in Config.PRO_USERS:
         is_in_gap, sleep_time = await check_time_gap(m.from_user.id)
         if is_in_gap:
-            await m.reply_text("Sorry Sir,\n"
-                               "No Flooding Allowed!\n\n"
-                               f"Send After `{str(sleep_time)}s` !!",
-                               quote=True)
+            await m.reply_text(f"Sorry Sir,\nNo Flooding Allowed!\n\nSend After `{str(sleep_time)}s` !!", quote=True)
             return
     await add_user_to_database(c, m)
     # Proceed
-    editable = await m.reply_text("Now send me new file name!", quote=True)
+    editable = await m.reply_text("Now send me a new file name!", quote=True)
     user_input_msg: Message = await c.listen(m.chat.id)
-    if user_input_msg.text is None:
-        await editable.edit("Process Cancelled!")
-        return await user_input_msg.continue_propagation()
-    if user_input_msg.text and user_input_msg.text.startswith("/"):
+    if user_input_msg.text is None or user_input_msg.text.startswith("/"):
         await editable.edit("Process Cancelled!")
         return await user_input_msg.continue_propagation()
     _raw_file_name = get_media_file_name(m.reply_to_message)
@@ -61,9 +51,7 @@ async def rename_handler(c: Client, m: Message):
         _default_thumb_ = await db.get_thumbnail(m.from_user.id)
         if not _default_thumb_:
             _m_attr = get_file_attr(m.reply_to_message)
-            _default_thumb_ = _m_attr.thumbs[0].file_id \
-                if (_m_attr and _m_attr.thumbs) \
-                else None
+            _default_thumb_ = _m_attr.thumbs[0].file_id if (_m_attr and _m_attr.thumbs) else None
         await handle_not_big(c, m, get_media_file_id(m.reply_to_message), file_name,
                              editable, get_file_type(m.reply_to_message), _default_thumb_)
         return
@@ -76,18 +64,11 @@ async def rename_handler(c: Client, m: Message):
             file_size=get_media_file_size(m.reply_to_message),
             file_name=file_name,
             progress=progress_for_pyrogram,
-            progress_args=(
-                "Uploading ...\n"
-                f"DC: {_c_file_id.dc_id}",
-                editable,
-                c_time
-            )
+            progress_args=("Uploading ...\n" f"DC: {_c_file_id.dc_id}", editable, c_time)
         )
         if not file_id:
-            return await editable.edit("Failed to Rename!\n\n"
-                                       "Maybe your file corrupted :(")
+            return await editable.edit("Failed to Rename!\n\nMaybe your file is corrupted :(")
         await handle_big_rename(c, m, file_id, file_name, editable, file_type)
     except Exception as err:
-        await editable.edit("Failed to Rename File!\n\n"
-                            f"**Error:** `{err}`\n\n"
-                            f"**Traceback:** `{traceback.format_exc()}`")
+        await editable.edit(f"Failed to Rename File!\n\n**Error:** `{err}`\n\n**Traceback:** `{traceback.format_exc()}`")
+        
