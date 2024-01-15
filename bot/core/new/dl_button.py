5ed656ca1f93e069d318e2c455b84cfa5f5f2915
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-01-15 15:14:32 +0530

    Update dl_button.py

--- bot/core/new/dl_button.py
+++ bot/core/new/dl_button.py
@@ -11,10 +11,10 @@ import requests
 import math
 import time
 from urllib.parse import urlparse
-from info import *
+from config import *
 from datetime import datetime
 from Script import script
-from plugins.rlazy_thumbnail import *
+from bot.plugins.thumbnail import *
 from pyrogram import enums
 from bot.core.db.database import db
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
