53b2f9d021d4053e2a68bcd4365eef2712093038
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-01-19 23:13:49 +0530

    Update metadata.py

--- bot/plugins/metadata.py
+++ bot/plugins/metadata.py
@@ -94,27 +94,27 @@ async def Edit_Metadata(c: Client, m: Message):
         return
     try: os.remove(the_media)
     except: pass
-	file_size = os.path.getsize(stream)
-	if (int(file_size) > 2097152000) and (Config.ALLOW_UPLOAD_TO_STREAMTAPE is True) and (Config.STREAMTAPE_API_USERNAME != "NoNeed") and (Config.STREAMTAPE_API_PASS != "NoNeed"):
-		await editable.edit(f"Sorry Sir,\n\nFile Size Become {humanbytes(file_size)} !!\nI can't Upload to Telegram!\n\nSo Now Uploading to Streamtape ...")
-		try:
-			async with aiohttp.ClientSession() as session:
-				Main_API = "https://api.streamtape.com/file/ul?login={}&key={}"
-				hit_api = await session.get(Main_API.format(Config.STREAMTAPE_API_USERNAME, Config.STREAMTAPE_API_PASS))
-				json_data = await hit_api.json()
-				temp_api = json_data["result"]["url"]
-				files = {'file1': open(stream, 'rb')}
-				response = await session.post(temp_api, data=files)
-				data_f = await response.json(content_type=None)
-				download_link = data_f["result"]["url"]
-				filename = stream.split("/")[-1].replace("_"," ")
-				text_edit = f"File Uploaded to Streamtape!\n\n**File Name:** `{filename}`\n**Size:** `{humanbytes(file_size)}`\n**Link:** `{download_link}`"
-				await editable.edit(text_edit, parse_mode="Markdown", disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Open Link", url=download_link)]]))
-		except Exception as e:
-			print(f"Error: {e}")
-			await editable.edit("Sorry, Something went wrong!\n\nCan't Upload to Streamtape. You can report at [Support Group](https://t.me/linux_repo).")
-		await rm_dir(root_dl_loc)
-		return
+    file_size = os.path.getsize(stream)
+    if (int(file_size) > 2097152000) and (Config.ALLOW_UPLOAD_TO_STREAMTAPE is True) and (Config.STREAMTAPE_API_USERNAME != "NoNeed") and (Config.STREAMTAPE_API_PASS != "NoNeed"):
+        await editable.edit(f"Sorry Sir,\n\nFile Size Become {naturalsize(file_size)} !!\nI can't Upload to Telegram!\n\nSo Now Uploading to Streamtape ...")
+        try:
+            async with aiohttp.ClientSession() as session:
+                Main_API = "https://api.streamtape.com/file/ul?login={}&key={}"
+                hit_api = await session.get(Main_API.format(Config.STREAMTAPE_API_USERNAME, Config.STREAMTAPE_API_PASS))
+                json_data = await hit_api.json()
+                temp_api = json_data["result"]["url"]
+                files = {'file1': open(stream, 'rb')}
+                response = await session.post(temp_api, data=files)
+                data_f = await response.json(content_type=None)
+                download_link = data_f["result"]["url"]
+                filename = stream.split("/")[-1].replace("_"," ")
+                text_edit = f"File Uploaded to Streamtape!\n\n**File Name:** `{filename}`\n**Size:** `{naturalsize(file_size)}`\n**Link:** `{download_link}`"
+                await editable.edit(text_edit, parse_mode="Markdown", disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Open Link", url=download_link)]]))
+        except Exception as e:
+            print(f"Error: {e}")
+            await editable.edit("Sorry, Something went wrong!\n\nCan't Upload to Streamtape. You can report at [Support Group](https://t.me/linux_repo).")
+        await rm_dir(root_dl_loc)
+        return
     upload_as_doc = await db.get_upload_as_doc(m.from_user.id)
     _default_thumb_ = await db.get_thumbnail(m.from_user.id)
     if not _default_thumb_:
