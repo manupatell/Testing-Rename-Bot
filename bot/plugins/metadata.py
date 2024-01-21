0f8f3359def76c5254c3ed642db2a88c7383bd2d
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-01-21 19:14:53 +0530

    Update metadata.py

--- bot/plugins/metadata.py
+++ bot/plugins/metadata.py
@@ -48,10 +48,10 @@ async def Edit_Metadata(c: Client, m: Message):
         file_name = user_input_msg.text.rsplit(".", 1)[0][:60] + "." + default_f_name.rsplit(".", 1)[-1].lower()
     else:
         new_file_name = user_input_msg.text[:60]
-    await editable.edit_text("Please Wait ...")
+    await editable.edit("Please Wait ...")
     newfile_name = f"{default_f_name.rsplit('.', 1)[0] if default_f_name else 'output'}.mkv"
     await editable.edit("Downloading Video ...")
-    dl_loc = Config.DOWNLOAD_DIR + "/" + str(m.from_user.id) + "/" + str(m.message.id) + "/"
+    dl_loc = Config.DOWNLOAD_DIR + "/" + str(m.from_user.id) + "/" + str(m.message_id) + "/"
     root_dl_loc = dl_loc
     stream = f"{dl_loc}{new_file_name}"
     if not os.path.isdir(dl_loc):
