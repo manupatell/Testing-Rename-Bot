db2db5ee8fdd16459c6503fdda56d2d9b2d93a93
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-01-23 07:02:19 +0530

    Update file_info.py

--- bot/core/file_info.py
+++ bot/core/file_info.py
@@ -1,5 +1,36 @@
 from pyrogram.types import Message
 
+def get_media_file_name(message: Message):
+    media = (
+        message.audio or
+        message.document or
+        message.sticker or
+        message.video or
+        message.animation
+    )
+
+    if media and media.file_name:
+        return media.file_name
+    else:
+        return None
+
+def get_media_file_size(message: Message):
+    media = (
+        message.audio or
+        message.document or
+        message.photo or
+        message.sticker or
+        message.video or
+        message.animation or
+        message.voice or
+        message.video_note
+    )
+
+    if media and media.file_size:
+        return media.file_size
+    else:
+        return None
+
 def get_media_mime_type(message: Message):
     """
     Pass Message object of audio or document or video to get mime_type
