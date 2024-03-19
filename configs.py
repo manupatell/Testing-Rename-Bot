db6469b270dfb638927f52b9c246c46d55fa3562
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-03-19 19:40:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 class Config(object):
     API_ID = int(os.environ.get("API_ID", "11973721"))
     API_HASH = os.environ.get("API_HASH", "5264bf4663e9159565603522f58d3c18")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6809904281:AAGdbV-NFrCtnNhwhXoBuXdDJf-evqRUkec")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6172666504:AAE7NHAasDmnXn1oBvcjHVh28aQqZhYDnOc")
     DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./Downloads")
     LOGGER = logging
     OWNER_ID = int(os.environ.get("OWNER_ID", 1391556668))
@@ -24,4 +24,3 @@ class Config(object):
     STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "zM4WB3RxQeIogb")
     STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", "d1cf7f6a67162eacdd77")
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
-    
