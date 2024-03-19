e814079ceb0f23623c5ebca22a0e34b6c2f5019a
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-03-19 20:54:49 +0530

    Update metadata.py

--- bot/plugins/metadata.py
+++ bot/plugins/metadata.py
@@ -24,48 +24,44 @@ from bot.core.file_info import get_file_attr
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
-@Client.on_message(filters.private & (filters.video | filters.document | filters.audio))
-async def video_info_handler(c: Client, m: Message):
-    default_f_name = get_media_file_name(m)
-    title = (await db.get_title(m.from_user.id)) or "StarMovies.hop.sh"
-    caption = await db.get_caption(m.from_user.id)
+@Client.on_message(filters.command("edit_metadata") & filters.private & ~filters.edited)
+async def Edit_Metadata(c: Client, m: Message):
+    default_f_name = get_media_file_name(m.reply_to_message)
+    title = (await db.get_titles(m.from_user.id)) or "StarMovies.hop.sh"
     await add_user_to_database(c, m)
-    if m.from_user.id not in Config.PRO_USERS:
-        is_in_gap, sleep_time = await check_time_gap(m.from_user.id)
-        if is_in_gap:
-            await m.reply_text("**Sorry Sir,**\n"
-                               "**No Flooding Allowed!**\n\n"
-                               f"**Send After `{str(sleep_time)}s`..!!**",
-                               quote=True)
-            return
-    await add_user_to_database(c, m)
-    editable = await m.reply_text(f"**Current File Name :-** `{default_f_name}` \n**File Caption :-** {caption}\n**Current Title :-** `{title}` \n**You Can Change Your Settings from /settings Command.\nNow Send me New File Name..!**", quote=True)
+    if not m.reply_to_message:
+        await m.reply_text(f"Reply to video with,\n/{m.command[0]}", True)
+        return
+    editable = await m.reply_text("Now send me new file name! Current Title is {title}", quote=True)
     user_input_msg: Message = await c.listen(m.chat.id)
     if user_input_msg.text is None:
-        await editable.edit("**Process Cancelled!**")
+        await editable.edit("Process Cancelled!")
         return await user_input_msg.continue_propagation()
     if user_input_msg.text and user_input_msg.text.startswith("/"):
-        await editable.edit("**Process Cancelled!**")
+        await editable.edit("Process Cancelled!")
         return await user_input_msg.continue_propagation()
     if user_input_msg.text.rsplit(".", 1)[-1].lower() != default_f_name.rsplit(".", 1)[-1].lower():
         file_name = user_input_msg.text.rsplit(".", 1)[0][:255] + "." + default_f_name.rsplit(".", 1)[-1].lower()
     else:
-        new_file_name = user_input_msg.text
+        new_file_name = user_input_msg.text[:60]
     await editable.edit("Please Wait ...")
     newfile_name = f"{default_f_name.rsplit('.', 1)[0] if default_f_name else 'output'}.mkv"
-    await editable.edit("**Downloading Video...**")
-    dl_loc = Config.DOWNLOAD_DIR + "/" + str(m.from_user.id) + "/"
+    file_type = m.reply_to_message.video or m.reply_to_message.document
+    if not file_type.mime_type.startswith("video/"):
+        await m.reply_text("This is not a Video!", True)
+        return
+    await editable.edit("Downloading Video ...")
+    dl_loc = Config.DOWNLOAD_DIR + "/" + str(m.from_user.id) + "/" + str(m.message_id) + "/"
     root_dl_loc = dl_loc
-    stream = shlex.quote(dl_loc + new_file_name) # Config.DOWNLOAD_DIR + "/" + str(m.from_user.id) + "/", f"{new_file_name}"
     if not os.path.isdir(dl_loc):
         os.makedirs(dl_loc)
     c_time = time.time()
     the_media = await c.download_media(
-        message=m,
+        message=m.reply_to_message,
         file_name=dl_loc,
-        progress=display_progress_for_pyrogram,
+        progress=progress_for_pyrogram,
         progress_args=(
-            "**Downloading...**",
+            "Downloading ...",
             editable,
             c_time
         )
@@ -82,12 +78,12 @@ async def video_info_handler(c: Client, m: Message):
         if title:
             middle_cmd += f' -metadata title="{title}"'
         for stream in details["streams"]:
-            if (stream["codec_type"] == "video") and video_title:
-                middle_cmd += f' -metadata:s:{stream["index"]} title="{video_title}"'
-            elif (stream["codec_type"] == "audio") and audio_title:
-                middle_cmd += f' -metadata:s:{stream["index"]} title="{audio_title}"'
-            elif (stream["codec_type"] == "subtitle") and subtitle_title:
-                middle_cmd += f' -metadata:s:{stream["index"]} title="{subtitle_title}"'
+            if (stream["codec_type"] == "video") and title:
+                middle_cmd += f' -metadata:s:{stream["index"]} title="{title}"'
+            elif (stream["codec_type"] == "audio") and title:
+                middle_cmd += f' -metadata:s:{stream["index"]} title="{title}"'
+            elif (stream["codec_type"] == "subtitle") and title:
+                middle_cmd += f' -metadata:s:{stream["index"]} title="{title}"'
         dl_loc = dl_loc + str(time.time()).replace(".", "") + "/"
         if not os.path.isdir(dl_loc):
             os.makedirs(dl_loc)
@@ -126,3 +122,4 @@ async def video_info_handler(c: Client, m: Message):
             thumb=_default_thumb_ or None
         )
     await rm_dir(root_dl_loc)
+    
