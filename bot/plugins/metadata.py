b51f826cc13f45bcb64f0d63aa73d5dce6be533d
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-03-19 22:32:12 +0530

    Update metadata.py

--- bot/plugins/metadata.py
+++ bot/plugins/metadata.py
@@ -101,14 +101,14 @@ async def Edit_Metadata(c: Client, m: Message):
     if _default_thumb_:
         _default_thumb_ = await c.download_media(_default_thumb_, root_dl_loc)
     if (not upload_as_doc) and m.video:
-        await c.upload_video(
+        await c.send_video(
             chat_id=m.chat.id,
             video=f"{dl_loc}{new_file_name}",
             thumb=_default_thumb_ or None,
             editable_message=editable,
         )
     else:
-        await c.upload_document(
+        await c.send_document(
             chat_id=m.chat.id,
             document=f"{dl_loc}{new_file_name}",
             editable_message=editable,
