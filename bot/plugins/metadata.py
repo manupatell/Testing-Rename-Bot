ed88272993c929f2517e7ded9441495e04e0cf75
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-03-19 20:04:11 +0530

    Update metadata.py

--- bot/plugins/metadata.py
+++ bot/plugins/metadata.py
@@ -30,42 +30,42 @@ async def video_info_handler(c: Client, m: Message):
     title = (await db.get_title(m.from_user.id)) or "StarMovies.hop.sh"
     caption = await db.get_caption(m.from_user.id)
     await add_user_to_database(c, m)
-    if (not m.reply_to_message) or (len(m.command) == 1):
-        await m.reply_text(f"Reply to video with,\n/{m.command[0]} `--change-title` new title `--change-video-title` new video title `--change-audio-title` new audio title `--change-subtitle-title` new subtitle title `--change-file-name` new file name", True)
-        return
-    default_f_name = get_media_file_name(m.reply_to_message)
-    new_file_name = f"{default_f_name.rsplit('.', 1)[0] if default_f_name else 'output'}.mkv"
-    if len(m.command) <= 1:
-        return
-
-    flags = [i.strip() for i in m.text.split('--')]
-    for f in flags:
-        if "change-file-name" in f:
-            new_file_name = f[len("change-file-name"):].strip().rsplit(".", 1)[0] + ".mkv"
-        if "change-title" in f:
-            title = f[len("change-title"):].strip()
-        if "change-video-title" in f:
-            video_title = f[len("change-video-title"):].strip()
-        if "change-audio-title" in f:
-            audio_title = f[len("change-audio-title"):].strip()
-        if "change-subtitle-title" in f:
-            subtitle_title = f[len("change-subtitle-title"):].strip()
-    file_type = m.reply_to_message.video or m.reply_to_message.document
-    if not file_type.mime_type.startswith("video/"):
-        await m.reply_text("This is not a Video!", True)
-        return
-    editable = await m.reply_text("Downloading Video ...", quote=True)
-    dl_loc = Config.DOWNLOAD_DIR + "/" + str(m.from_user.id) + "/" + str(m.message_id) + "/"
+    if m.from_user.id not in Config.PRO_USERS:
+        is_in_gap, sleep_time = await check_time_gap(m.from_user.id)
+        if is_in_gap:
+            await m.reply_text("**Sorry Sir,**\n"
+                               "**No Flooding Allowed!**\n\n"
+                               f"**Send After `{str(sleep_time)}s`..!!**",
+                               quote=True)
+            return
+    await add_user_to_database(c, m)
+    editable = await m.reply_text(f"**Current File Name :-** `{default_f_name}` \n**File Caption :-** {caption}\n**Current Title :-** `{title}` \n**You Can Change Your Settings from /settings Command.\nNow Send me New File Name..!**", quote=True)
+    user_input_msg: Message = await c.listen(m.chat.id)
+    if user_input_msg.text is None:
+        await editable.edit("**Process Cancelled!**")
+        return await user_input_msg.continue_propagation()
+    if user_input_msg.text and user_input_msg.text.startswith("/"):
+        await editable.edit("**Process Cancelled!**")
+        return await user_input_msg.continue_propagation()
+    if user_input_msg.text.rsplit(".", 1)[-1].lower() != default_f_name.rsplit(".", 1)[-1].lower():
+        file_name = user_input_msg.text.rsplit(".", 1)[0][:255] + "." + default_f_name.rsplit(".", 1)[-1].lower()
+    else:
+        new_file_name = user_input_msg.text
+    await editable.edit("Please Wait ...")
+    newfile_name = f"{default_f_name.rsplit('.', 1)[0] if default_f_name else 'output'}.mkv"
+    await editable.edit("**Downloading Video...**")
+    dl_loc = Config.DOWNLOAD_DIR + "/" + str(m.from_user.id) + "/"
     root_dl_loc = dl_loc
+    stream = shlex.quote(dl_loc + new_file_name) # Config.DOWNLOAD_DIR + "/" + str(m.from_user.id) + "/", f"{new_file_name}"
     if not os.path.isdir(dl_loc):
         os.makedirs(dl_loc)
     c_time = time.time()
     the_media = await c.download_media(
-        message=m.reply_to_message,
+        message=m,
         file_name=dl_loc,
-        progress=progress_for_pyrogram,
+        progress=display_progress_for_pyrogram,
         progress_args=(
-            "Downloading ...",
+            "**Downloading...**",
             editable,
             c_time
         )
