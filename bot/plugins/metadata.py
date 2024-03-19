a16fdc70f26d3fd570d402614bc6c595069e7cd4
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-03-19 19:57:06 +0530

    Update metadata.py

--- bot/plugins/metadata.py
+++ bot/plugins/metadata.py
@@ -25,114 +25,105 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
 @Client.on_message(filters.private & (filters.video | filters.document | filters.audio))
-async def Edit_Metadata(c: Client, m: Message):
-    default_f_name = get_media_file_name(m)
-    title = (await db.get_title(m.from_user.id)) or "StarMovies.hop.sh"
-    caption = await db.get_caption(m.from_user.id)
-    if m.from_user.id not in Config.PRO_USERS:
-        is_in_gap, sleep_time = await check_time_gap(m.from_user.id)
-        if is_in_gap:
-            await m.reply_text("**Sorry Sir,**\n"
-                               "**No Flooding Allowed!**\n\n"
-                               f"**Send After `{str(sleep_time)}s`..!!**",
-                               quote=True)
-            return
+async def video_info_handler(c: Client, m: Message):
     await add_user_to_database(c, m)
-    editable = await m.reply_text(f"**Current File Name :-** `{default_f_name}` \n**File Caption :-** {caption}\n**Current Title :-** `{title}` \n**You Can Change Your Settings from /settings Command.\nNow Send me New File Name..!**", quote=True)
-    user_input_msg: Message = await c.listen(m.chat.id)
-    if user_input_msg.text is None:
-        await editable.edit("**Process Cancelled!**")
-        return await user_input_msg.continue_propagation()
-    if user_input_msg.text and user_input_msg.text.startswith("/"):
-        await editable.edit("**Process Cancelled!**")
-        return await user_input_msg.continue_propagation()
-    if user_input_msg.text.rsplit(".", 1)[-1].lower() != default_f_name.rsplit(".", 1)[-1].lower():
-        file_name = user_input_msg.text.rsplit(".", 1)[0][:255] + "." + default_f_name.rsplit(".", 1)[-1].lower()
-    else:
-        new_file_name = user_input_msg.text
-    await editable.edit("Please Wait ...")
-    newfile_name = f"{default_f_name.rsplit('.', 1)[0] if default_f_name else 'output'}.mkv"
-    await editable.edit("**Downloading Video...**")
-    dl_loc = Config.DOWNLOAD_DIR + "/" + str(m.from_user.id) + "/"
+    if (not m.reply_to_message) or (len(m.command) == 1):
+        await m.reply_text(f"Reply to video with,\n/{m.command[0]} `--change-title` new title `--change-video-title` new video title `--change-audio-title` new audio title `--change-subtitle-title` new subtitle title `--change-file-name` new file name", True)
+        return
+    title = None
+    video_title = None
+    audio_title = None
+    subtitle_title = None
+    default_f_name = get_media_file_name(m.reply_to_message)
+    new_file_name = f"{default_f_name.rsplit('.', 1)[0] if default_f_name else 'output'}.mkv"
+    if len(m.command) <= 1:
+        return
+
+    flags = [i.strip() for i in m.text.split('--')]
+    for f in flags:
+        if "change-file-name" in f:
+            new_file_name = f[len("change-file-name"):].strip().rsplit(".", 1)[0] + ".mkv"
+        if "change-title" in f:
+            title = f[len("change-title"):].strip()
+        if "change-video-title" in f:
+            video_title = f[len("change-video-title"):].strip()
+        if "change-audio-title" in f:
+            audio_title = f[len("change-audio-title"):].strip()
+        if "change-subtitle-title" in f:
+            subtitle_title = f[len("change-subtitle-title"):].strip()
+    file_type = m.reply_to_message.video or m.reply_to_message.document
+    if not file_type.mime_type.startswith("video/"):
+        await m.reply_text("This is not a Video!", True)
+        return
+    editable = await m.reply_text("Downloading Video ...", quote=True)
+    dl_loc = Config.DOWNLOAD_DIR + "/" + str(m.from_user.id) + "/" + str(m.message_id) + "/"
     root_dl_loc = dl_loc
-    stream = shlex.quote(dl_loc + new_file_name) # Config.DOWNLOAD_DIR + "/" + str(m.from_user.id) + "/", f"{new_file_name}"
     if not os.path.isdir(dl_loc):
         os.makedirs(dl_loc)
     c_time = time.time()
     the_media = await c.download_media(
-        message=m,
+        message=m.reply_to_message,
         file_name=dl_loc,
-        progress=display_progress_for_pyrogram,
+        progress=progress_for_pyrogram,
         progress_args=(
-            "**Downloading...**",
+            "Downloading ...",
             editable,
             c_time
         )
     )
-    await editable.edit("**Trying to Fetch Media Metadata...**")
+    await editable.edit("Trying to Fetch Media Metadata ...")
     output = await execute(f"ffprobe -hide_banner -show_streams -print_format json {shlex.quote(the_media)}")
     if not output:
         await rm_dir(root_dl_loc)
-        return await editable.edit("**Can't Fetch Media Info!**")
+        return await editable.edit("Can't fetch media info!")
+
     try:
         details = json.loads(output[0])
         middle_cmd = f"ffmpeg -i {shlex.quote(the_media)} -c copy -map 0"
         if title:
             middle_cmd += f' -metadata title="{title}"'
         for stream in details["streams"]:
-            if (stream["codec_type"] == "video") and title:
-                middle_cmd += f' -metadata:s:{stream["index"]} title="{title}"'
-            elif (stream["codec_type"] == "audio") and title:
-                middle_cmd += f' -metadata:s:{stream["index"]} title="{title}"'
-            elif (stream["codec_type"] == "subtitle") and title:
-                middle_cmd += f' -metadata:s:{stream["index"]} title="{title}"'
+            if (stream["codec_type"] == "video") and video_title:
+                middle_cmd += f' -metadata:s:{stream["index"]} title="{video_title}"'
+            elif (stream["codec_type"] == "audio") and audio_title:
+                middle_cmd += f' -metadata:s:{stream["index"]} title="{audio_title}"'
+            elif (stream["codec_type"] == "subtitle") and subtitle_title:
+                middle_cmd += f' -metadata:s:{stream["index"]} title="{subtitle_title}"'
         dl_loc = dl_loc + str(time.time()).replace(".", "") + "/"
         if not os.path.isdir(dl_loc):
             os.makedirs(dl_loc)
         middle_cmd += f" {shlex.quote(dl_loc + new_file_name)}"
-        await editable.edit("**Please Wait ...\n\nProcessing Video...**")
+        await editable.edit("Please Wait ...\n\nProcessing Video ...")
         await execute(middle_cmd)
-        await editable.edit("**Renamed Successfully!**")
+        await editable.edit("Renamed Successfully!")
     except:
         # Clean Up
-        await editable.edit("**Failed to Process Video!**")
+        await editable.edit("Failed to process video!")
         await rm_dir(root_dl_loc)
         return
+    try: os.remove(the_media)
+    except: pass
     upload_as_doc = await db.get_upload_as_doc(m.from_user.id)
     _default_thumb_ = await db.get_thumbnail(m.from_user.id)
     if not _default_thumb_:
-        _m_attr = get_file_attr(m)
+        _m_attr = get_file_attr(m.reply_to_message)
         _default_thumb_ = _m_attr.thumbs[0].file_id \
             if (_m_attr and _m_attr.thumbs) \
             else None
-    file_size = get_media_file_size(m)
-    file_caption = await db.get_caption(m.from_user.id)
-    if file_caption:
-         try:
-             caption = file_caption.format(file_name=new_file_name, file_size=file_size, duration=convert(duration))
-         except Exception as e:
-             await editable.edit(text=f"**Your caption Error Unexpected Keyword ●> {e}**")
-             return 
-    else:
-        caption = f"**{new_file_name}**"
     if _default_thumb_:
         _default_thumb_ = await c.download_media(_default_thumb_, root_dl_loc)
-    if (not upload_as_doc) and m.video:
-        await c.send_video(
+    if (not upload_as_doc) and m.reply_to_message.video:
+        await c.upload_video(
             chat_id=m.chat.id,
             video=f"{dl_loc}{new_file_name}",
-            caption=caption,
-            duration=duration,
             thumb=_default_thumb_ or None,
             editable_message=editable,
         )
     else:
-        await c.send_document(
+        await c.upload_document(
             chat_id=m.chat.id,
             document=f"{dl_loc}{new_file_name}",
-            caption=caption,
-            duration=duration,
-            thumb=_default_thumb_ or None,
             editable_message=editable,
+            thumb=_default_thumb_ or None
         )
     await rm_dir(root_dl_loc)
