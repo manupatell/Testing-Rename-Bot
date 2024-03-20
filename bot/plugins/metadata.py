0380094dcec85f12d931204ae0adc9de84bcbe81
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-03-20 17:57:24 +0530

    Update metadata.py

--- bot/plugins/metadata.py
+++ bot/plugins/metadata.py
@@ -27,25 +27,12 @@ from hachoir.parser import createParser
 @Client.on_message(filters.command("edit_metadata") & filters.private)
 async def video_info_handler(c: Client, m: Message):
     await add_user_to_database(c, m)
-    if not m.reply_to_message:
-        await m.reply_text(f"Reply to video with,\n/{m.command[0]}", True)
+    if (not m.reply_to_message) or (len(m.command) == 1):
+        await m.reply_text(f"Reply to video with,\n/{m.command[0]} file_name", True)
         return
     title = (await db.get_title(m.from_user.id)) or "StarMovies.hop.sh"
     default_f_name = get_media_file_name(m.reply_to_message)
     new_file_name = f"{default_f_name.rsplit('.', 1)[0] if default_f_name else 'output'}.mkv"
-    editable = await m.reply_text("Now send me new file name! Current Title is {title}", quote=True)
-    user_input_msg: Message = await c.listen(m.chat.id)
-    if user_input_msg.text is None:
-        await editable.edit("Process Cancelled!")
-        return await user_input_msg.continue_propagation()
-    if user_input_msg.text and user_input_msg.text.startswith("/"):
-        await editable.edit("Process Cancelled!")
-        return await user_input_msg.continue_propagation()
-    if user_input_msg.text.rsplit(".", 1)[-1].lower() != default_f_name.rsplit(".", 1)[-1].lower():
-        file_name = user_input_msg.text.rsplit(".", 1)[0][:255] + "." + default_f_name.rsplit(".", 1)[-1].lower()
-    else:
-        new_file_name = user_input_msg.text[:60]
-    await editable.edit("Please Wait ...")
     newfile_name = f"{default_f_name.rsplit('.', 1)[0] if default_f_name else 'output'}.mkv"
     file_type = m.reply_to_message.video or m.reply_to_message.document
     if not file_type.mime_type.startswith("video/"):
