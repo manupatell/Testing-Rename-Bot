314adead3a69a1ef414d5f40d98c082b67b4356d
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-03-20 19:05:32 +0530

    Update metadata.py

--- bot/plugins/metadata.py
+++ bot/plugins/metadata.py
@@ -43,6 +43,7 @@ async def video_info_handler(c: Client, m: Message):
     for f in flags:
         if "change-file-name" in f:
             file_name_text = f[len("change-file-name"):].strip().rsplit(".", 1)[0][:60]
+            caption = f[len("change-file-name"):].strip().rsplit(".", 1)[0] + ".mkv"
             new_file_name = f"{file_name_text}.mkv"
         if "change-title" in f:
             title = f[len("change-title"):].strip()
