b43feae3a704a5fad7a53f98c519a6d88de91175
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-05-03 07:32:53 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -6,14 +6,14 @@ from bot.core.db.add import add_user_to_database
 @Client.on_message(filters.command("start") & filters.private)
 async def ping_handler(c: Client, m: "types.Message"):
     if not m.from_user:
-        return await m.reply_text("**I don't know about you, sar.**")
+        return await m.reply_text("**I don't know about you, sir.**")
     await add_user_to_database(c, m)
     await c.send_message(
         chat_id=m.chat.id,
-        text="**Hi, I am Rename Bot!**\n\n"
-             "I can rename media without downloading it!\n"
-             "Speed depends on your media DC.\n\n"
-             "Just send me media and reply to it with /rename command.",
+        text="**Hi, I am Star Bots Official Metadata Editor Bot!**\n\n"
+             "**I can rename media without downloading it!**\n"
+             "**Speed depends on your media DC.**\n\n"
+             "**Just send me media and reply to it with /edit_metadata command.**",
         reply_markup=types.InlineKeyboardMarkup([[
             types.InlineKeyboardButton("Show Settings ⚙️",
                                        callback_data="showSettings")
@@ -27,12 +27,12 @@ async def help_handler(c: Client, m: "types.Message"):
     await add_user_to_database(c, m)
     await c.send_message(
         chat_id=m.chat.id,
-        text="**I can rename media without downloading it!**\n"
-             "Speed depends on your media DC.\n\n"
-             "Just send me media and reply to it with /rename command.\n\n"
-             "To set custom thumbnail, reply to any image with /set_thumbnail\n\n"
-             "To see custom thumbnail, press /show_thumbnail",
+        text="**I Can Rename Media Without Downloading It!**\n"
+             "**Speed Depends on Your Media DC.**\n\n"
+             "**Just send me media and reply to it with /edit_metadata command.**\n\n"
+             "**To set custom thumbnail, reply to any image with /set_thumbnail**\n\n"
+             "**To see custom thumbnail, press /show_thumbnail**",
         reply_markup=types.InlineKeyboardMarkup([[
-            types.InlineKeyboardButton("Show Settings",
+            types.InlineKeyboardButton("Show Settings ⚙️",
                                        callback_data="showSettings")]])
     )
