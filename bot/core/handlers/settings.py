22cb1eea4e67e1678384fce24f0bcbc814291715
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-05-09 05:06:44 +0530

    Update settings.py

--- bot/core/handlers/settings.py
+++ bot/core/handlers/settings.py
@@ -1,5 +1,5 @@
 import asyncio
-from pyrogram import types, errors, enums
+from pyrogram import Client, filters, types, errors, enums
 from configs import Config
 from bot.core.db.database import db
 
@@ -53,3 +53,62 @@ async def show_settings(m: "types.Message"):
         await show_settings(m)
     except Exception as err:
         Config.LOGGER.getLogger(__name__).error(err)
+
+# Settings Command 
+
+@Client.on_message(filters.command("settings") & filters.private)
+async def settings_command_handler(c: Client, m: types.Message):
+    usr_id = m.chat.id
+    user_data = await db.get_user_data(usr_id)
+    if not user_data:
+        await m.edit("Failed to fetch your data from database!")
+        return
+    upload_as_doc = user_data.get("upload_as_doc", False)
+    caption = user_data.get("caption", None)
+    apply_caption = user_data.get("apply_caption", True)
+    thumbnail = user_data.get("thumbnail", None)
+    title = user_data.get("title", None)
+    buttons_markup = [
+        [types.InlineKeyboardButton(f"Upload as Doc {'✅' if upload_as_doc else '❌'}",
+                                    callback_data="triggerUploadMode")],
+        [types.InlineKeyboardButton(f"Apply Caption {'✅' if apply_caption else '❌'}",
+                                    callback_data="triggerApplyCaption")],
+        [types.InlineKeyboardButton(f"Apply Default Caption {'❌' if caption else '✅'}",
+                                    callback_data="triggerApplyDefaultCaption")],
+        [types.InlineKeyboardButton(f"{'Change' if caption else 'Set'} Custom Caption",
+                                    callback_data="setCustomCaption")],
+        [types.InlineKeyboardButton(f"{'Change' if title else 'Set'} Title",
+                                    callback_data="setTitle")],
+        [types.InlineKeyboardButton(f"{'Change' if thumbnail else 'Set'} Thumbnail",
+                                    callback_data="setThumbnail")]
+    ]
+    if thumbnail:
+        buttons_markup.append([types.InlineKeyboardButton("Show Thumbnail",
+                                                          callback_data="showThumbnail")])
+    if title:
+        buttons_markup.append([types.InlineKeyboardButton("Show Title",
+                                                          callback_data="showTitle")])
+    if caption:
+        buttons_markup.append([types.InlineKeyboardButton("Show Caption",
+                                                          callback_data="showCaption")])
+    buttons_markup.append([types.InlineKeyboardButton("Close Message",
+                                                      callback_data="closeMessage")])
+
+    try:
+        await m.reply(
+            text="**Here You Can setup Your ⚙️ Settings:**",
+            reply_markup=types.InlineKeyboardMarkup(buttons_markup),
+            disable_web_page_preview=True,
+            parse_mode=enums.ParseMode.MARKDOWN
+        )
+    except errors.FloodWait as e:
+        await asyncio.sleep(e.x)
+        await m.reply(
+            text="**Here You Can setup Your ⚙️ Settings:**",
+            reply_markup=types.InlineKeyboardMarkup(buttons_markup),
+            disable_web_page_preview=True,
+            parse_mode=enums.ParseMode.MARKDOWN
+        )
+    except Exception as err:
+        Config.LOGGER.getLogger(__name__).error(err)
+        
