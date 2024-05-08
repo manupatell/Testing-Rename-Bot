4ff22922b109ebfa38734e06d87864854f1da6b6
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-05-09 05:22:14 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -12,7 +12,7 @@ async def ping_handler(c: Client, m: "types.Message"):
         chat_id=m.chat.id,
         text="**Hi, I am Star Bots Official Metadata Editor Bot!**\n\n"
              "**I can rename media without downloading it!**\n"
-             "**Speed depends on your media DC.**\n\n"
+             "**Speed depends on your media DC. Change Your ⚙️ Settings in /settings Command.**\n\n"
              "**Just send me media and reply to it with /edit_metadata command.**",
         reply_markup=types.InlineKeyboardMarkup([[
             types.InlineKeyboardButton("Show Settings ⚙️",
@@ -28,7 +28,7 @@ async def help_handler(c: Client, m: "types.Message"):
     await c.send_message(
         chat_id=m.chat.id,
         text="**I Can Rename Media Without Downloading It!**\n"
-             "**Speed Depends on Your Media DC.**\n\n"
+             "**Speed Depends on Your Media DC. Change Your ⚙️ Settings in /settings Command.**\n\n"
              "**Just send me media and reply to it with /edit_metadata command.**\n\n"
              "**To set custom thumbnail, reply to any image with /set_thumbnail**\n\n"
              "**To see custom thumbnail, press /show_thumbnail**",
